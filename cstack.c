7d426287224dbbb6ffa3290181142e9790524d5c
Egor Maleev <e9126515199@icloud.com>
2023-12-05 09:31:32 +0500

    my first commit

--- cstack.c
+++ cstack.c
@@ -1,42 +1,116 @@
-#include "cstack.h"
-#include <stddef.h>
+﻿#include <iostream>
 
-#define UNUSED(VAR) (void)(VAR)
+int i = 0;
+typedef struct node* stack_t;
+typedef int hstack_t;
+typedef struct stack_entry stack_entry_t;
 
-hstack_t stack_new(void)
+struct stack_entry //структура, описывающая конкретный стек
 {
-    return -1;
+    int reserved; //индикация, используется ли сейчас данный элемент таблицы стека(то есть конкретный стек); показывает число элементов в стеке
+    stack_t stack; //верхушка стека - указатель на верхний узел
+};
+
+struct stack_entries_table //структура, представляющая собой таблицу стеков
+{
+    unsigned int size; //размер таблицы(количество стеков)
+    stack_entry_t* entries; //указатель на конкретный стек
+};
+
+struct stack_entries_table g_table[20];
+
+struct node //структура, описывающая конкретный узел данного стека
+{
+    struct node* prev; // указатель на предыдущий элемент
+    unsigned int size; //размер данных
+    const char* data[0]; // поле данных
+};
+
+hstack_t stack_new(void)  //создание нового стека
+{
+    ++i;
+    stack_entry* p = (stack_entry*)malloc(sizeof(stack_entry)); // указатель на конкретный стек
+    p->reserved = 0; //стек активен
+    p->stack = NULL;
+    g_table[i - 1].size = i;
+    g_table[i - 1].entries = p;
+    return(i - 1);
+}
+//стек создавался на удаленном месте??????????????
+//data[0]
+// malloc and realloc
+
+int stack_valid_handler(const hstack_t stack)
+{
+    if (stack >= i)
+    {
+        return 1;
+    }
+    if (g_table[stack].entries->reserved == -1)
+    {
+        return 1;
+    }
+    return(0);
 }
 
-void stack_free(const hstack_t hstack)
+void stack_free(const hstack_t stack) //удаление стека
 {
-    UNUSED(hstack);
+    if (stack_valid_handler(stack) == 1)
+    {
+        exit;
+    }
+    stack_entry* p = g_table[stack].entries; // указатель на конкретный стек
+    p->reserved = -1; //стек неактивен
 }
 
-int stack_valid_handler(const hstack_t hstack)
+unsigned int stack_size(const hstack_t stack)
 {
-    UNUSED(hstack);
-    return 1;
+    if (stack_valid_handler(stack) == 1)
+    {
+        exit;
+    }
+    stack_entry* p = g_table[stack].entries;
+    return(p->reserved);
 }
 
-unsigned int stack_size(const hstack_t hstack)
+void stack_push(const hstack_t stack, const void* data, const unsigned int size)
 {
-    UNUSED(hstack);
-    return 0;
+    if (data == NULL)
+    {
+        exit;
+    }
+    if (size == 0)
+    {
+        exit;
+    }
+    if (stack_valid_handler(stack) == 1)
+    {
+        exit;
+    }
+    stack_entry* p = g_table[stack].entries;//указатель на данный стек, в который мы пушаем элемент
+    node* lst = (struct node*)malloc(sizeof(node) + size); // выделение памяти под узел
+    const char* data_char = (const char*)malloc(sizeof(data)); // приведение входных данных типа const void* к типу const char*
+    lst->data[size - 1] = data_char; //запись данных
+    lst->prev = p->stack; //переставляем указатель на предыдущий элемент
+    p->stack = lst;//переставляем указатель на вершину стека
+    --(p->reserved);
 }
 
-void stack_push(const hstack_t hstack, const void* data_in, const unsigned int size)
+unsigned int stack_pop(const hstack_t stack, void* data_out, const unsigned int size)
 {
-    UNUSED(hstack);
-    UNUSED(data_in);
-    UNUSED(size);
+    if (stack_valid_handler(stack) == 1)
+    {
+        exit;
+    }
+    node* lst = g_table[stack].entries->stack;
+    unsigned int razmer = lst->size;
+    stack_entry* p = g_table[stack].entries;
+    p->stack = lst->prev; //переставляем указатель на вершину стека
+    data_out = (void*)lst->data[razmer - 1];
+    node* free(lst);
+    return(razmer);
 }
-
-unsigned int stack_pop(const hstack_t hstack, void* data_out, const unsigned int size)
+int main()
 {
-    UNUSED(hstack);
-    UNUSED(data_out);
-    UNUSED(size);
-    return 0;
+    stack_new();
 }
-
